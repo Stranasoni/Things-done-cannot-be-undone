15fd940b50926d645f8197411fca113df513d0b5
Soni <Dsn2004@mail.ru>
2024-02-24 23:05:19 +0300

    The creation of shaders is placed in a separate class

---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <glad/glad.h>
+
+namespace Renderer {
+	class ShaderProgram {
+	public:
+		ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+		~ShaderProgram();
+		bool isCompiled() const { return m_isCompiled; }
+		void use() const;
+
+		ShaderProgram() = delete;
+		ShaderProgram(ShaderProgram&) = delete;
+		ShaderProgram& operator=(const ShaderProgram&) = delete;
+		ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
+		ShaderProgram(ShaderProgram&& shaderProgram)noexcept;
+	private:
+		bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
+		bool m_isCompiled = false;
+		GLuint m_ID = 0;
+	};
+}
